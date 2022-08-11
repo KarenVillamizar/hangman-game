a05593564ec193da721209ee4473bd9bdf704be5
Karen Villamizar <villamizark816@gmail.com>
2022-08-10 22:17:06 -0500

    Adivina palabra solo la palabra "cama"

--- hangman-game.py
+++ hangman-game.py
@@ -2,12 +2,27 @@ from itertools import combinations_with_replacement
 
 
 def run():
-    print("Prueba llave ssh")
-
-
-
-
-
+    word = "cama"
+    #w = list(enumerate(word))
+    n = len(word)
+    dif = n
+    lines = []
+    c2 = 0
+    for i in range(0, n):
+            lines.append('_ ')
+    while dif != 0:
+        print("Adivina la palabra :)")
+        print(*lines)
+        letter = input("ingresa una letra:")
+        c = 0
+        for i in word:
+            c = c + 1
+            if letter == i:
+                c2 = c2 + 1 
+                lines[c-1] = letter
+        dif = n - c2
+    print(*lines)
+    print("Ganaste!")
 
 
 
